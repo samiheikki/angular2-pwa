97aa5664e9d5850a2352923d720431fc8f422ab4
Sami Suo-Heikki <samiheikki@vaadin.com>
2016-08-22 19:05:06 +0300

    Install material2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { APP_SHELL_DIRECTIVES } from '@angular/app-shell';
 
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+
 @Component({
   moduleId: module.id,
   selector: 'app-root',
@@ -8,9 +10,10 @@ import { APP_SHELL_DIRECTIVES } from '@angular/app-shell';
   <h1>
     {{title}}
   </h1>
+  <button md-raised-button>RAISED</button>
   `,
   styles: [],
-  directives: [APP_SHELL_DIRECTIVES]
+  directives: [APP_SHELL_DIRECTIVES, MD_BUTTON_DIRECTIVES]
 })
 export class AppComponent {
   title = 'app works!';
--- src/system-config.ts
+++ src/system-config.ts
@@ -9,11 +9,35 @@
  **********************************************************************************************/
 /** Map relative paths to URLs. */
 const map: any = {
+  '@angular2-material': 'vendor/@angular2-material'
 };
 
 /** User packages configuration. */
 const packages: any = {
 };
+
+const materialPkgs:string[] = [
+  'button',
+  'button-toggle',
+  'core',
+  'card',
+  'checkbox',
+  'grid-list',
+  'icon',
+  'input',
+  'list',
+  'progress-bar',
+  'progress-circle',
+  'radio',
+  'sidenav',
+  'slide-toggle',
+  'tabs',
+  'toolbar'
+];
+
+materialPkgs.forEach((pkg) => {
+  packages[`@angular2-material/${pkg}`] = {main: `${pkg}.js`};
+});
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /***********************************************************************************************
