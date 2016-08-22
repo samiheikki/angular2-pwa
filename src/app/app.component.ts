4917c40257c2ce44766cb776914adf3ccfd00b52
Sami Suo-Heikki <samiheikki@vaadin.com>
2016-08-22 20:25:31 +0300

    Work with material elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,53 @@
 import { Component } from '@angular/core';
 import { APP_SHELL_DIRECTIVES } from '@angular/app-shell';
-
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import { MdToolbar } from '@angular2-material/toolbar';
+import { MdSpinner } from '@angular2-material/progress-circle';
+import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
+
   template: `
-  <h1>
-    {{title}}
-  </h1>
-  <button md-raised-button>RAISED</button>
+  <md-sidenav-layout>
+    <md-sidenav #nav mode="over">
+      nav Side Drawer
+    </md-sidenav>
+
+    <div>
+      <md-toolbar color="primary">
+        <md-icon>home</md-icon>
+        <span>My App</span>
+      </md-toolbar>
+
+      <md-spinner *shellRender></md-spinner>
+      <h1 *shellNoRender>App is Fully Rendered</h1>
+
+      <h1>My Content</h1>
+
+      <button md-button (click)="nav.toggle()">Toggle nav Side Drawer</button>
+    </div>
+  </md-sidenav-layout>
   `,
-  styles: [],
-  directives: [APP_SHELL_DIRECTIVES, MD_BUTTON_DIRECTIVES]
+
+  styles: [`
+    md-spinner {
+      margin: 24px auto 0;
+    }
+  `],
+
+  directives: [
+    APP_SHELL_DIRECTIVES,
+    MdToolbar,
+    MdSpinner,
+    MD_SIDENAV_DIRECTIVES,
+    MD_BUTTON_DIRECTIVES,
+    MdIcon
+  ],
+  providers: [MdIconRegistry]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'My app';
 }
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
 import { APP_SHELL_RUNTIME_PROVIDERS } from '@angular/app-shell';
+import { HTTP_PROVIDERS } from '@angular/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent, [ APP_SHELL_RUNTIME_PROVIDERS ]);
+bootstrap(AppComponent, [ APP_SHELL_RUNTIME_PROVIDERS, HTTP_PROVIDERS ]);
